6c8ac4dfe7f7c477125281ef8aea64054137f937
dimitarkolev00 <61474565+dimitarkolev00@users.noreply.github.com>
2020-02-25 22:22:46 +0200

    Add files via upload

---
+++ 01.Arrays/P03.Magic Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace Magic_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNum = nums[i];
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (currentNum + nums[j] == sum)
+                    {
+                        Console.WriteLine(currentNum + " " + nums[j]);
+                    }
+
+                }
+            }
+
+        }
+    }
+}
