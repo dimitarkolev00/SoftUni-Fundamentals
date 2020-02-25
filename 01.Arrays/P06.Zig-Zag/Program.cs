df7be299f80508bb606fbf6a9b38edca650e02b6
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-02-25 22:50:34 +0200

    Initial push

---
+++ 01.Arrays/P06.Zig-Zag/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace Zig_Zag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[] arrOne = new int[n];
+            int[] arrTwo = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] nums = Console.ReadLine()
+                    .Split(" ")
+                    .Select(int.Parse)
+                    .ToArray();
+                if ((i + 1) % 2 != 0)
+                {
+                    arrOne[i] = nums[0];
+                    arrTwo[i] = nums[1];
+                }
+                else
+                {
+                    arrOne[i] = nums[1];
+                    arrTwo[i] = nums[0];
+                }
+            }
+            Console.WriteLine(string.Join(" ", arrOne));
+            Console.WriteLine(string.Join(" ", arrTwo));
+        }
+    }
+}
