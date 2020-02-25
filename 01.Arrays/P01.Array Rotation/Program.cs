4d8bb83f690b395aa85d3534ebc34cc111b397cf
dimitarkolev00 <61474565+dimitarkolev00@users.noreply.github.com>
2020-02-25 22:12:28 +0200

    Add files via upload

---
+++ 01.Arrays/P01.Array Rotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_Rotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] arr = Console.ReadLine().Split(' ');
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i < n; i++)
+            {
+                string firstNum = arr[0];
+
+                for (int index = 1; index < arr.Length; index++)
+                {
+                    string currentElement = arr[index];
+                    arr[index - 1] = currentElement;
+
+                }
+                arr[arr.Length - 1] = firstNum;
+            }
+            
+           
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
