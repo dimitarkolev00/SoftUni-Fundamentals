b79f0b77eb57350c4dcc6842a18bfcd78264f493
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-02-26 23:00:22 +0200

    Initial push

---
+++ 03.Associative arrays/P09.SoftUni Exam Results/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace _10._SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> students = new Dictionary<string, int>();
+            Dictionary<string, int> submissions = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "exam finished")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split("-");
+                string user = tokens[0];
+
+                if (tokens.Length > 2)
+                {
+                    string language = tokens[1];
+                    int points = int.Parse(tokens[2]);
+                    if (!students.ContainsKey(user))
+                    {
+                        students.Add(user, points);
+                    }
+
+                    else
+                    {
+                        if (students[user] < points)
+                        {
+                            students[user] = points;
+                        }
+
+                    }
+                    if (!submissions.ContainsKey(language))
+                    {
+                        submissions.Add(language, 0);
+                    }
+                    submissions[language]++;
+
+                }
+                else
+                {
+                    if (students.ContainsKey(user))
+                    {
+                        students.Remove(user);
+                    }
+                }
+            }
+            Console.WriteLine("Results:");
+            foreach (var student in students
+                .OrderByDescending(s => s.Value)
+                .ThenBy(s => s.Key))
+            {
+                Console.WriteLine($"{student.Key} | {student.Value }");
+            }
+
+            Console.WriteLine("Submissions:");
+            foreach (var s in submissions.OrderBy(s => s.Key).ThenBy(s => s.Value))
+            {
+                Console.WriteLine($"{s.Key} - {s.Value}");
+            }
+        }
+    }
+}
