5dd5f41ac8d1e1de63cfe2493f2eb0839e268844
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-02-26 22:56:39 +0200

    Initial push

---
+++ 03.Associative arrays/P01.Count Chars in a String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Count_Chars_in_a_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] input = Console.ReadLine().ToCharArray();
+            Dictionary<char, int> occurances = new Dictionary<char, int>();
+
+            foreach (char letter in input)
+            {
+                if (letter != ' ')
+                {
+                    if (!occurances.ContainsKey(letter))
+                    {
+                        occurances.Add(letter, 0);
+                    }
+                    occurances[letter]++;
+                }
+
+            }
+            foreach (var c in occurances)
+            {
+                Console.WriteLine($"{c.Key} -> {c.Value}");
+            }
+        }
+    }
+}
