530aab02982aab4568845e77214a79c3d460232b
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-03-01 12:35:20 +0200

    Initial push

---
+++ Exam Prep 1/02.Message Decrypter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Message_Decrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"^([$%])([A-Z][a-z]{2,})\1\:\s\[(\d+)\]\|\[(\d+)\]\|\[(\d+)\]\|$";
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = Regex.Match(input, pattern);
+                if (match.Success)
+                {
+                    string tag = match.Groups[2].Value;
+                    string message = String.Empty;
+                    for (int j = 3; j < match.Groups.Count; j++)
+                    {
+                        int val = int.Parse(match.Groups[j].Value);
+                        message += (char)(val);
+                        
+                    }
+                    Console.WriteLine($"{tag}: {message}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+        }
+    }
+}
---
+++ Exam Prep 1/03.Messages Manager/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Messages_Manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> sendedMessages = new Dictionary<string, int>();
+            Dictionary<string, int> recievedMessages = new Dictionary<string, int>();
+
+            int capacity = int.Parse(Console.ReadLine());
+            string command = Console.ReadLine();
+
+            while (command != "Statistics")
+            {
+                string[] cmdArgs = command.Split('=');
+                string cmdType = cmdArgs[0];
+                if (cmdType == "Add")
+                {
+                    string username = cmdArgs[1];
+                    int sent = int.Parse(cmdArgs[2]);
+                    int recieved = int.Parse(cmdArgs[3]);
+
+                    if (!sendedMessages.ContainsKey(username))
+                    {
+                        sendedMessages.Add(username, sent);
+                        recievedMessages.Add(username, recieved);
+                    }
+
+                }
+                else if (cmdType == "Message")
+                {
+                    string sender = cmdArgs[1];
+                    string reciever = cmdArgs[2];
+
+                    if (sendedMessages.ContainsKey(sender) 
+                        && recievedMessages.ContainsKey(reciever))
+                    {
+                        sendedMessages[sender]++;
+                        recievedMessages[reciever]++;
+                        int senderTotalMessages = sendedMessages[sender] + recievedMessages[sender];
+
+                        if (senderTotalMessages>=capacity)
+                        {
+                            sendedMessages.Remove(sender);
+                            recievedMessages.Remove(sender);
+
+                            Console.WriteLine($"{sender} reached the capacity!");
+                        }
+
+                        int recieverTotalMessages = sendedMessages[reciever] + recievedMessages[reciever];
+
+                        if (recieverTotalMessages>=capacity)
+                        {
+                            sendedMessages.Remove(reciever);
+                            recievedMessages.Remove(reciever);
+                            Console.WriteLine($"{reciever} reached the capacity!");
+                        }
+                    }
+
+                }
+                else if (cmdType=="Empty")
+                {
+                    string username = cmdArgs[1];
+                    if (username=="All")
+                    {
+                        sendedMessages.Clear();
+                        recievedMessages.Clear();
+                    }
+                    else
+                    {
+                        if (sendedMessages.ContainsKey(username))
+                        {
+                            sendedMessages.Remove(username);
+                            recievedMessages.Remove(username);
+                        }
+                        
+                    }            
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"Users count: {sendedMessages.Count}");
+
+            recievedMessages = recievedMessages.OrderByDescending(kvp => kvp.Value)
+                .ThenBy(kvp => kvp.Key)
+                .ToDictionary(a => a.Key, b => b.Value);
+
+            foreach (var kvp in recievedMessages)
+            {
+                string username = kvp.Key;
+                int totalMessages = kvp.Value + sendedMessages[username];
+
+                Console.WriteLine($"{username} - {totalMessages}");
+            }
+        }
+    }
+}
