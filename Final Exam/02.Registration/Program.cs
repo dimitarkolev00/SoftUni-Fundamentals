896bd2380d22de6d822fbf8afd63b0ccf5d0fff7
Dimitar Kolev <dimitarkolev00@gmail.com>
2020-03-09 21:58:48 +0200

    Initial push

---
+++ Final Exam/02.Registration/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"([U$]+)([A-Z][a-z]{2,})\1([P@$]+)([a-z]{5,}[\d]+)(\3)";
+            Regex regex = new Regex(pattern);
+
+            int n = int.Parse(Console.ReadLine());
+            int successCount = 0; 
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+                
+                if (match.Success)
+                {
+                    string username = match.Groups[2].Value;
+                    string password = match.Groups[4].Value;
+                    Console.WriteLine("Registration was successful");
+                    Console.WriteLine($"Username: {username}, Password: {password}");
+                    successCount++;
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+            Console.WriteLine($"Successful registrations: {successCount}");
+        }
+    }
+}
---
+++ Final Exam/03.Inbox Manager/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary<string, string> username = new Dictionary<string, string>();
+            Dictionary<string, List<string>> sentEmails = new Dictionary<string, List<string>>();
+            string command = Console.ReadLine();
+
+            while (command != "Statistics")
+            {
+                string[] cmdArgs = command.Split("->");
+                string cmdType = cmdArgs[0];
+                string usernameCheck = cmdArgs[1];
+
+                if (cmdType == "Add")
+                {
+
+                    if (!sentEmails.ContainsKey(usernameCheck))
+                    {
+                        sentEmails.Add(usernameCheck, new List<string>());
+                       
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{usernameCheck} is already registered");
+                    }
+
+
+                }
+                else if (cmdType == "Send")
+                {
+                    string email = cmdArgs[2];
+                    sentEmails[usernameCheck].Add(email);
+                }
+                else if (cmdType == "Delete")
+                {
+                    if (!sentEmails.ContainsKey(usernameCheck))
+                    {
+                        Console.WriteLine($"{ usernameCheck} not found!");
+                    }
+                    else
+                    {
+                        sentEmails.Remove(usernameCheck);
+                      
+                    }
+
+                }
+
+                command = Console.ReadLine();
+
+            }
+            Console.WriteLine($"Users count: {sentEmails.Count}");
+
+            
+            foreach (var kvp in sentEmails.OrderByDescending(u=>u.Value.Count).ThenBy(u=>u.Key))
+            {
+                
+                Console.WriteLine($"{kvp.Key}");
+                foreach (var emails in kvp.Value)
+                {
+                    Console.WriteLine($" - {emails}");
+                }
+
+            }
+        }
+    }
+}
